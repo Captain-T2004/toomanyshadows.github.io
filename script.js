65f476221295f6da84fd51fba31aa7306389a04c
Captain-T2004 <126911424+Captain-T2004@users.noreply.github.com>
2023-03-30 21:52:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function Redirect(){
+    window.location.href = "./login.html";
+}
+function NOSITE(){
+    alert("THERE IS NO SITE. I guess you must leave... you should leave. I mean you can inspect the code but there is no site so just leave.....")
+}
